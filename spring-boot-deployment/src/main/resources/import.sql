32aa69d0e450f7987fe13419bfb2d392b6a87a12
rijosam <rijosam19@gmail.com>
2025-06-07 16:18:37 +0200

    Add import.sql for data initialization

---
+++ spring-boot-deployment/src/main/resources/import.sql
@@ -0,0 +1,18 @@
+-- This file allow to write SQL commands that will be emitted in test and dev.
+
+INSERT INTO quote (text, character) VALUES ('They don’t know that we know they know we know.', 'Phoebe');
+INSERT INTO quote (text, character) VALUES ('Oh, I wish I could, but I don’t want to.', 'Phoebe');
+INSERT INTO quote (text, character) VALUES ('I don''t even have a ''pla.', 'Phoebe');
+INSERT INTO quote (text, character) VALUES ('I’m not great at the advice. Can I interest you in a sarcastic comment?', 'Chandler');
+INSERT INTO quote (text, character) VALUES ('So it seems like this Internet thing’s here to stay.', 'Chandler');
+INSERT INTO quote (text, character) VALUES ('Hey, come on, I say more dumb things before 9 a.m. than most people say all day.', 'Chandler');
+INSERT INTO quote (text, character) VALUES ('Could I be wearing any more clothes?', 'Joey');
+INSERT INTO quote (text, character) VALUES ('You can’t just give up. Is that what a dinosaur would do?', 'Joey');
+INSERT INTO quote (text, character) VALUES ('How you doin''?', 'Joey');
+INSERT INTO quote (text, character) VALUES ('We were on a break!', 'Ross');
+INSERT INTO quote (text, character) VALUES ('I''m fine. Totally fine.', 'Ross');
+INSERT INTO quote (text, character) VALUES ('I grew up with Monica. If you didn''t eat fast, you didn''t eat.', 'Ross');
+INSERT INTO quote (text, character) VALUES ('Welcome to the real world. It sucks. You’re gonna love it.', 'Monica');
+INSERT INTO quote (text, character) VALUES ('I know!', 'Monica');
+INSERT INTO quote (text, character) VALUES ('Well, maybe I don’t need your money. Wait, wait, I said maybe!', 'Rachel');
+INSERT INTO quote (text, character) VALUES ('Oh I''m sorry. Did my back hurt your knife', 'Rachel');
