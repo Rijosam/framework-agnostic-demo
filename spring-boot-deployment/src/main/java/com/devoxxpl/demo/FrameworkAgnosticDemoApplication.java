c5179e26720ae4509d4ddfe89e20a77b3fc252af
rijosam <rijosam19@gmail.com>
2025-06-12 07:40:35 +0200

    Update spring boot annotations

--- spring-boot-deployment/src/main/java/com/devoxxpl/demo/FrameworkAgnosticDemoApplication.java
+++ spring-boot-deployment/src/main/java/com/devoxxpl/demo/FrameworkAgnosticDemoApplication.java
@@ -3,11 +3,9 @@ package com.devoxxpl.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.devoxxpl"})
-@EnableJpaRepositories(basePackages = "com.devoxxpl.database.repository")
-@EntityScan(basePackages = "com.devoxxpl.database.entity")
+@SpringBootApplication(scanBasePackages = {"com.devoxxpl.demo"})
+@EntityScan(basePackages = "com.devoxxpl.database.entity") //for initializing the database using import.sql
 public class FrameworkAgnosticDemoApplication {
 
     public static void main(String[] args) {
